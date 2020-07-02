02c02d95715e06a66bc109aa7e68fff402674438
Dusan Stankovic <sdusan@gmail.com>
2020-07-02 18:54:45 +0200

    Created Customer controller and view. Closes #20.

---
+++ src/main/java/dev/dstankovic/musicstore/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package dev.dstankovic.musicstore.controller;
+
+import dev.dstankovic.musicstore.service.CustomerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/list")
+    public String listCustomers(Model model) {
+
+        model.addAttribute("customers", customerService.findAll());
+
+        return "list-customers";
+    }
+}
