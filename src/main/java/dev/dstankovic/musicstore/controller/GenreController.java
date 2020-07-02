b7fb6dc2e32163872c61c5d1e050fd0d3a552876
Dusan Stankovic <sdusan@gmail.com>
2020-07-02 18:13:10 +0200

    Created Genre controller and view. Closes #14.

---
+++ src/main/java/dev/dstankovic/musicstore/controller/GenreController.java
@@ -0,0 +1,27 @@
+package dev.dstankovic.musicstore.controller;
+
+import dev.dstankovic.musicstore.service.GenreService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/genres")
+public class GenreController {
+
+    private GenreService genreService;
+
+    public GenreController(GenreService genreService) {
+
+        this.genreService = genreService;
+    }
+
+    @GetMapping("/list")
+    public String listGenres(Model model) {
+
+        model.addAttribute("genres", genreService.findAll());
+
+        return "list-genres";
+    }
+}
