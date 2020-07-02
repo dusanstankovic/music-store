d8526fbe845b47faf7d95b24874b550a5c96c024
Dusan Stankovic <sdusan@gmail.com>
2020-07-02 18:04:29 +0200

    Created Track controller and view. Closes #12.

---
+++ src/main/java/dev/dstankovic/musicstore/controller/TrackController.java
@@ -0,0 +1,27 @@
+package dev.dstankovic.musicstore.controller;
+
+import dev.dstankovic.musicstore.service.TrackService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tracks")
+public class TrackController {
+
+    private TrackService trackService;
+
+    public TrackController(TrackService trackService) {
+
+        this.trackService = trackService;
+    }
+
+    @GetMapping("/list")
+    public String listTracks(Model model) {
+
+        model.addAttribute("tracks", trackService.findAll());
+
+        return "list-tracks";
+    }
+}
