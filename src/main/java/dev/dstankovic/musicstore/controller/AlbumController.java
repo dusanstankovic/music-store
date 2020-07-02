ba5aa04901c6aa972ed81d90aca37cffac63ed79
Dusan Stankovic <sdusan@gmail.com>
2020-07-02 17:41:05 +0200

    Created Album controller and view. Closes #8.

---
+++ src/main/java/dev/dstankovic/musicstore/controller/AlbumController.java
@@ -0,0 +1,27 @@
+package dev.dstankovic.musicstore.controller;
+
+import dev.dstankovic.musicstore.service.AlbumService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/albums")
+public class AlbumController {
+
+    private AlbumService albumService;
+
+    public AlbumController(AlbumService albumService) {
+
+        this.albumService = albumService;
+    }
+
+    @GetMapping("list")
+    public String listAlbums(Model model) {
+
+        model.addAttribute("albums", albumService.findAll());
+
+        return "list-albums";
+    }
+}
