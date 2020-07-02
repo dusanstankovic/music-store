9e73572fa03481eda6f15864fddf30e3fc13d919
Dusan Stankovic <sdusan@gmail.com>
2020-07-02 22:30:19 +0200

    Created Invoice controller and view. Closes #22.

---
+++ src/main/java/dev/dstankovic/musicstore/controller/InvoiceController.java
@@ -0,0 +1,27 @@
+package dev.dstankovic.musicstore.controller;
+
+import dev.dstankovic.musicstore.service.InvoiceService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/invoices")
+public class InvoiceController {
+
+    private InvoiceService invoiceService;
+
+    public InvoiceController(InvoiceService invoiceService) {
+
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/list")
+    public String listInvoices(Model model) {
+
+        model.addAttribute("invoices", invoiceService.findAll());
+
+        return "list-invoices";
+    }
+}
