5b7573606f3f61ec0364429aaf266b4394b6e44d
Dusan Stankovic <sdusan@gmail.com>
2020-07-04 12:11:57 +0200

    Added code for adding, saving, and deleting Genre. Closes #47.

--- src/main/java/dev/dstankovic/musicstore/controller/GenreController.java
+++ src/main/java/dev/dstankovic/musicstore/controller/GenreController.java
@@ -1,10 +1,10 @@
 package dev.dstankovic.musicstore.controller;
 
+import dev.dstankovic.musicstore.entity.Genre;
 import dev.dstankovic.musicstore.service.GenreService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/genres")
@@ -23,5 +23,39 @@ public class GenreController {
         model.addAttribute("genres", genreService.findAll());
 
         return "/genres/list-genres";
+    }
+
+    @GetMapping("/addGenre")
+    public String addGenre(Model model) {
+
+        Genre genre = new Genre();
+        model.addAttribute("genre", genre);
+
+        return "/genres/genre-form";
+    }
+
+    @PostMapping("/save")
+    public String saveGenre(@ModelAttribute("genre") Genre genre) {
+
+        genreService.save(genre);
+
+        return "redirect:/genres/list";
+    }
+
+    @GetMapping("/updateGenre")
+    public String updateGenre(@RequestParam("genreId") int id, Model model) {
+
+        Genre genre = genreService.findById(id);
+        model.addAttribute("genre", genre);
+
+        return "/genres/genre-form";
+    }
+
+    @GetMapping("/delete")
+    public String deleteGenre(@RequestParam("genreId") int id) {
+
+        genreService.deleteById(id);
+
+        return "redirect:/genres/list";
     }
 }
